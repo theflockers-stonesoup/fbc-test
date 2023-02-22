dc23014bea9bdd0d66feb52115b8512ff1bc3027
Scott Hebert <shebert@redhat.com>
2023-02-22 14:39:01 -0500

    update path for opm command

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -4,13 +4,13 @@ echo "Current OPM version is: $(opm version)"
 echo "Requires OPM v1.26.3+"
 
 # step0
-opm alpha render-veneer basic components/v4.13-step0.yaml > 4.13/catalog/catalog.yaml
+opm alpha render-veneer basic components/v4.13-step0.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
 
 # step1
-# opm alpha render-veneer basic components/v4.13-step1.yaml > 4.13/catalog/catalog.yaml
+# opm alpha render-veneer basic components/v4.13-step1.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
 
 # step2
-# opm alpha render-veneer basic components/v4.13-step2.yaml > 4.13/catalog/catalog.yaml
+# opm alpha render-veneer basic components/v4.13-step2.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
 
 # step3
-# opm alpha render-veneer basic components/v4.13-step3.yaml > 4.13/catalog/catalog.yaml
+# opm alpha render-veneer basic components/v4.13-step3.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
