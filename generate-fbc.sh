f3c39967c3174336b9ccb4527d9ccc53612a7620
Scott Hebert <shebert@redhat.com>
2023-02-22 20:47:16 -0500

    parameterize step in script

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -3,14 +3,9 @@
 echo "Current OPM version is: $(opm version)"
 echo "Requires OPM v1.26.3+"
 
-# step0
-#opm alpha render-veneer basic components/v4.13-step0.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
+if [ -z "$1" ]; then
+  echo "usage: <script> <step #>"
+  exit 1
+fi
 
-# step1
-opm alpha render-veneer basic components/v4.13-step1.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
-
-# step2
-# opm alpha render-veneer basic components/v4.13-step2.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
-
-# step3
-# opm alpha render-veneer basic components/v4.13-step3.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
+opm alpha render-veneer basic components/v4.13-${1}.yaml > 4.13/catalog/kubevirt-hyperconverged/catalog.yaml
